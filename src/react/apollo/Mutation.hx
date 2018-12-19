52f0668813f5cec79fc39ba64dd37afee3695d09
k <k@klabz.org>
2018-12-19 16:54:09 +0100

    0.1.1 - Update Mutation types

--- src/react/apollo/Mutation.hx
+++ src/react/apollo/Mutation.hx
@@ -1,6 +1,6 @@
 package react.apollo;
 
-import haxe.extern.EitherType;
+import haxe.Constraints.Function;
 import js.Promise;
 
 import apollo.cache.DataProxy;
@@ -10,8 +10,8 @@ import apollo.client.core.PureQueryOptions;
 import apollo.client.error.ApolloError;
 import apollo.link.FetchResult;
 import apollo.util.Noise;
-import react.ReactComponent;
 import graphql.ASTDefs.DocumentNode;
+import react.ReactComponent;
 
 typedef MutationResult<TData> = {
 	@:optional var data:TData;
@@ -26,19 +26,33 @@ typedef MutationUpdaterFn<T> = DataProxy -> FetchResult_1<T> -> Void;
 typedef MutationOptions<TData, TVariables> = {
 	@:optional var variables:TVariables;
 	@:optional var opitmisticResponse:{};
-	@:optional var refetchQueries:EitherType<Array<String>, EitherType<Array<PureQueryOptions>, RefetchQueriesProviderFn>>;
+	@:optional var refetchQueries:RefetchQueries;
 	@:optional var update:MutationUpdaterFn<TData>;
 }
 
-typedef MutationFn<TData, TVariables> = EitherType<?MutationOptions<TData, TVariables>->Promise<Noise>, ?MutationOptions<TData, TVariables>->Promise<FetchResult_1<TData>>>;
+@:coreType
+abstract RefetchQueries
+	from Array<String>
+	from Array<PureQueryOptions>
+	from RefetchQueriesProviderFn
+	{}
+
+@:callable
+abstract MutationFn<TData, TVariables>(Function)
+	from Void->Void
+	from Void->Promise<Noise>
+	from Void->Promise<FetchResult_1<TData>>
+	from ?MutationOptions<TData, TVariables>->Promise<Noise>
+	from ?MutationOptions<TData, TVariables>->Promise<FetchResult_1<TData>>
+	{}
 
 typedef MutationProps<TData, TVariables> = {
 	var mutation:DocumentNode;
-	var children:MutationFn<TData, TVariables>->MutationResult<TData>->ReactElement;
+	var children:MutationFn<TData, TVariables>->MutationResult<TData>->ReactFragment;
 	@:optional var ignoreResults:Bool;
 	@:optional var optimisticResponse:{};
 	@:optional var variables:TVariables;
-	@:optional var refetchQueries:EitherType<Array<String>, EitherType<Array<PureQueryOptions>, RefetchQueriesProviderFn>>;
+	@:optional var refetchQueries:RefetchQueries;
 	@:optional var update:MutationUpdaterFn<TData>;
 	@:optional var onCompleted:TData->Void;
 	@:optional var onError:ApolloError->Void;
@@ -48,4 +62,4 @@ typedef MutationProps<TData, TVariables> = {
 
 @:jsRequire('react-apollo', 'Mutation')
 extern class Mutation<TData, TVariables>
-	extends ReactComponentOfProps<MutationProps<TData, TVariables>> {}
+extends ReactComponentOfProps<MutationProps<TData, TVariables>> {}
